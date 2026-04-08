3009e6dc1fc18ff134a4b1ff84e6b1484eda8ffd
Nikhil Gupta <139880433+nikhil8424@users.noreply.github.com>
2026-04-08 09:07:11 +0530

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -1,31 +1,57 @@
-# pip install pandas tensorflow scikit-learn matplotlib
-
+pip install pandas numpy matplotlib scikit-learn tensorflow keras
 import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, normalize
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("your_dataset.csv")
+data = pd.read_csv("heart.csv")
 
-X = data.iloc[:,:-1]
-y = LabelEncoder().fit_transform(data.iloc[:,-1])
+le = LabelEncoder()
+data['target'] = le.fit_transform(data['target'])
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3)
+X = data.drop('target', axis=1)
+y = data['target']
 
-X_train,X_test = normalize(X_train), normalize(X_test)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
 
-model = Sequential([
-    Dense(10, activation='relu', input_dim=X.shape[1]),
-    Dense(len(set(y)), activation='softmax')
-])
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model = Sequential()
+model.add(Dense(16, activation='relu', input_dim=X.shape[1]))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
 
-history = model.fit(X_train,y_train,epochs=20,batch_size=16,verbose=0)
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=Adam(learning_rate=0.01),
+    metrics=['accuracy']
+)
 
-print("Train Acc:", model.evaluate(X_train,y_train,verbose=0)[1])
-print("Test Acc:", model.evaluate(X_test,y_test,verbose=0)[1])
+history = model.fit(
+    X_train, y_train,
+    epochs=20,
+    batch_size=16,
+    verbose=1
+)
 
-plt.plot(history.history['loss']); plt.show()
+train_acc = model.evaluate(X_train, y_train)[1]
+test_acc = model.evaluate(X_test, y_test)[1]
+
+print("Train Accuracy:", train_acc)
+print("Test Accuracy:", test_acc)
+
+plt.plot(history.history['loss'])
+plt.show()
+
+for layer in model.layers:
+    w, b = layer.get_weights()
+    print("\nLayer:", layer.name)
+    print("Weights:\n", w)
+    print("Biases:\n", b)
